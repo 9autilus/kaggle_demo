c38d5ed5c96fdc3ad2e9137ae58575c8ac2c7272
Govind <govindnh4cl@gmail.com>
2018-04-28 20:24:59 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='KaggleDemo',
+    version='1.0',
+    description='Add custom files in kaggle kernel',
+    author='Govind',
+    author_email='example@example.com',
+    packages=['kaggle_demo'],  #same as name
+)
